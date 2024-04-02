ca97142117806c4a26e34b8b125202f507963675
peupedroo <pedrosilvagouveia@gmail.com>
2024-04-02 09:58:36 -0300

    tentando organizar o arquivo python

---
+++ python/latim/exercicio_latim.py
@@ -0,0 +1,11 @@
+
+idade= int (input ("digite uma idade  " ))
+
+if 0 <= idade <= 12:
+    print("voce e uma crianca ")
+elif 13 <= idade<= 17:
+    print("vc e adolecente ")
+elif 18 <= idade<= 64:
+    print("vc e adulto")
+else :
+    print ("vc e idoso ")
---
+++ python/lista_3_estudar/estudar.py
@@ -0,0 +1,9 @@
+nome=str(input("digite um numero "))
+
+count=0
+
+for char in nome :
+    if char :
+        count += 1
+
+print ("esse e seu numero de letras .   ", count)
