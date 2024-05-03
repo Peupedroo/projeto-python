a2e53aba45d02841717e38a2499e7525d308ccc2
peupedroo <pedrosilvagouveia@gmail.com>
2024-05-03 20:44:34 -0300

    mandando atividade massa de dicionario

--- python/lista_4/dicionario3.py
+++ python/lista_4/dicionario3.py
@@ -2,13 +2,13 @@ pacientes={
     1001 : {"nome": "pedro","idade": 19,"telefone": "4799203003" },
     1002 : {"nome": "jagunco","idade": 10012,"telefone": "8992883928"},
     1003 : {"nome": "exroai","idade": 100121, "telefone": "92002293"},
-    1004 : {"nome": "carlos daniel","idade":123, "telefonr": "9922123"}
+    1004 : {"nome": "carlos daniel","idade":123, "telefone": "9922123"}
     
 }
 
 medicos= {
-    "m001" : {"nome": "macedo", "idade ": 41, "telefone": "83900990909", "especialista em ": "urologia"},
-    "m002" : {"nome": "macedo", "idade": 41, "telefone": "839009309409", "especialista em ": "neurologista"},
+    "m001" : {"nome": "macedo", "idade": 41, "telefone": "83900990909", "especialista em": "urologia"},
+    "m002" : {"nome": "macedo", "idade": 41, "telefone": "839009309409", "especialista em": "neurologista"},
     "m003" : {"nome": "macedo", "idade": 89 , "telefone": "83998899889","especialista em": "estomatologia" }
 }
 
@@ -18,6 +18,14 @@ agenda={
     "ag003" : {"paciente": 1003,"medico": "m001", "data": "12/05/2024", "hora" : "18:24" }
 
 }
+print ("pacientes")
+for paciente_id, paciente_info in pacientes.items():
+     print(f"ID: {paciente_id} NOME: {paciente_info["nome"]} IDADE: {paciente_info["idade"]} TELEFONE: {paciente_info["telefone"]}")
 
-for i, j in pacientes,medicos,agenda.items():
-    print(f"{i}, {j}")
+
+for agenda_id, agenda_info in agenda.items():
+    paciente_id = agenda_info.get ("paciente")
+    paciente_info = pacientes.get(paciente_id)
+    
+    print(f"ID: {agenda_id}, paciente: {paciente_info["nome"]}")
+
